d39353ce37a2f281d4be8a10dcdc98b38180a175
Oussama JAMIL <oussamajamil321@gmail.com>
2025-01-11 21:31:24 +0100

    Fixing the 404 Error on Refresh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <div className="footer bg-primary text-secondary font-primary py-2">
+        <div className="footer bg-primary text-secondary font-primary py-2 mt-4">
             <div className="container flex justify-between items-center">
             <p className="text-sm">© capstone project 2025</p>
             <Link className="text-5xl tracking-widest flex items-center font-black" to="/">TBR</Link>
