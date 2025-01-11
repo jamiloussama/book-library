cb71d95b39ef29086ec4e1532efbf0a50e1bcb4e
Oussama JAMIL <oussamajamil321@gmail.com>
2025-01-11 21:19:19 +0100

    slider responsive update

--- src/components/BookCarousel.jsx
+++ src/components/BookCarousel.jsx
@@ -55,7 +55,13 @@ function BookCarousel() {
                 slidesToScroll: 1,
               },
             },
-            
+            {
+                breakpoint: 500, 
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                },
+              },
           ],  
   };
   return (
